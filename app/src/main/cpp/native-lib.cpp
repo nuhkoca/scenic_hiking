59ade50e8aac57e2755e8cbce17769399a6f70fe
Nuh Koca <nuhkocaa@gmail.com>
2025-01-31 00:01:37 +0300

    Update native-lib.cpp

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,6 +5,6 @@ extern "C" JNIEXPORT jstring
 
 JNICALL
 Java_com_kpit_scenichiking_util_config_Keys_mapBoxKey(JNIEnv *env, jobject object) {
-    std::string mapBox_key = "pk.eyJ1IjoibnVoa29jYSIsImEiOiJjazBwODBhcHYwZmVsM25uNnYxMHIybzAyIn0.9QCILPbCrVOTe5x5UCQY9g";
+    std::string mapBox_key = "YOUR_API_KEY";
     return env->NewStringUTF(mapBox_key.c_str());
 }
